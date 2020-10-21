b7ef4a6b4226c0e27f7dc28446f415870572d06a
deepster <59594456+deepster@users.noreply.github.com>
2020-10-21 12:02:21 -0500

    Can delete one or all applications from db

--- App.js
+++ App.js
@@ -1,114 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import HomeScreen from './pages/HomeScreen';
+import NewApp from './pages/NewApp';
+import DeleteApp from './pages/DeleteApp';
+import UpdateApp from './pages/UpdateApp';
+import ViewApp from './pages/ViewApp';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+
+
+const Stack = createStackNavigator();
+
+
+export default function App(){
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName = "HomeScreen">
+    <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+            title: 'Home', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#f4511e', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+        }}
+      />
+      <Stack.Screen
+        name="NewApp"
+        component={NewApp}
+        options={{
+            title: 'New Application', 
+            headerStyle: {
+                backgroundColor: '#f4511e', 
+            },
+            headerTintColor: '#fff', 
+            headerTitleStyle: {
+                fontWeight: 'bold', 
+            },
+        }}
+      />
+      <Stack.Screen
+        name="DeleteApp"
+        component={DeleteApp}
+        options={{
+            title: 'Delete Application(s)', 
+            headerStyle: {
+                backgroundColor: '#f4511e', 
+            },
+            headerTintColor: '#fff', 
+            headerTitleStyle: {
+                fontWeight: 'bold', 
+            },
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+
+
+
+
+
+
+
+   /* <View style={styles.container}>
+      <Text>Open up App.js to start worokg on your app!</Text>
+      <StatusBar style="auto" />
+    </View>*/
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
---
+++ pages/NewApp.js
@@ -0,0 +1,137 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert,TouchableOpacity } from 'react-native';
+
+import { openDatabase } from "expo-sqlite";  
+
+const NewApp = ({ navigation }) => {
+    const db = openDatabase('table_applications.db')
+    let [title, setJobTitle] = useState('');
+    let [company, setJobCompany] = useState('');
+    let [deadline, setJobDeadline] = useState('');
+    let [applied, setJobApplied] = useState('');
+    let [link, setJobLink] = useState('');
+
+    let new_app = () => {
+    //console.log(title);
+     db.transaction(function(tx){
+        tx.executeSql(
+          //"create table if not exists DataTable (id integer primary key not null, column_1 int, column_2 int, column_3 text);",
+          'CREATE TABLE IF NOT EXISTS table_applications(job_id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(225), company VARCHAR(225), deadline VARCHAR(40), applied VARCHAR(40), link VARCHAR(225));',
+          []
+        );
+
+        tx.executeSql(
+          "INSERT INTO table_applications (title, company, deadline, applied, link) VALUES (?,?,?,?,?)",
+          [title, company, deadline, applied, link],
+          (tx, results) => {
+              if (results.rowsAffected > 0) { //CHECK: alert isn't working insert console.log here
+              console.log('ResultsAdded', results.rowsAffected);
+                Alert.alert(
+                  'Success',
+                  'You\'re Job Application was Successfully Submitted',
+                  [
+                    {
+                      text: 'Ok',
+                      onPress: () => navigation.navigate('HomeScreen'),
+                    },
+                  ],
+                  { cancelable: false }
+                );
+              } else Alert.alert(
+              'Error',
+              'Submission Failed');
+          }
+        );
+
+        tx.executeSql(
+          "select * from table_applications",
+          [],
+          (_, { rows: { _array } }) => console.log( _array),
+          () => console.log("error fetching")
+        );
+
+      });
+    };
+
+    return (
+      <>
+      <View style={styles.container}>
+          <Text style = {styles.header}> Job Title </Text>
+          <TextInput
+          placeholder = "Enter Job Title"
+          style = {styles.input}
+          onChangeText={(title) => setJobTitle(title)}  // <Text> {title} </Text>
+          />
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.container}>
+          <Text style = {styles.header}> Company </Text>
+          <TextInput
+          placeholder = "Enter Company Name"
+          style = {styles.input}
+          onChangeText={(company) => setJobCompany(company)}  // <Text> {title} </Text>
+          />
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.container}>
+          <Text style = {styles.header}> Deadline </Text>
+          <TextInput
+          placeholder = "Enter Application Deadline"
+          style = {styles.input}
+          onChangeText={(deadline) => setJobDeadline(deadline)}  // <Text> {title} </Text>
+          />
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.container}>
+          <Text style = {styles.header}> Applied </Text>
+          <TextInput
+          placeholder = "Enter Date Application was Submitted"
+          style = {styles.input}
+          onChangeText={(applied) => setJobApplied(applied)}  // <Text> {title} </Text>
+          />
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.container}>
+          <Text style = {styles.header}> Job Link </Text>
+          <TextInput
+          placeholder = "Enter Link to Application"
+          style = {styles.input}
+          onChangeText={(link) => setJobLink(link)}  // <Text> {title} </Text>
+          />
+          <TouchableOpacity style={styles.button} onPress={new_app}>
+            <Text>Submit</Text>
+          </TouchableOpacity>
+          <StatusBar style="auto" />
+        </View>
+        </>
+      );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input:{
+    backgroundColor: '#fff', 
+    padding:10, 
+    margin:10,
+    fontSize: 16
+    },
+    header:{
+     marginTop: 8,
+     textAlign: 'center',
+     fontSize: 24,
+     fontWeight: '600',
+     color: '#000'
+    },
+    button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+});
+export default NewApp
