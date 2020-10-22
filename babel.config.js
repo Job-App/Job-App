a6d53254488d1439c15c825c5a86205b4defc1b2
gyhyihe <47801373+gyhyihe@users.noreply.github.com>
2020-10-22 06:04:53 -0400

    tests complete for basic components

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,46 @@
+/*
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
 };
+*/
+
+module.exports = {
+  presets: [
+    '@babel/react',
+    'module:metro-react-native-babel-preset',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-function-bind',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '^react-native$': 'react-native-web',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: [
+        [
+          'transform-react-remove-prop-types',
+          {
+            mode: 'remove',
+            ignoreFilenames: ['node_modules'],
+          },
+        ],
+      ],
+    },
+  },
+}
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,11 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 configure({ adapter: new Adapter() });
+
+global.matchMedia = global.matchMedia || function() {
+  return {
+      matches : false,
+      addListener : function() {},
+      removeListener: function() {}
+  }
+}
