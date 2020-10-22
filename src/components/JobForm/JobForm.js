3a22f3f73d6086b08f14283f3c627534616304ba
erichan42 <erichan210hh@gmail.com>
2020-10-22 02:12:09 -0700

    Started on implementeding sqlite to 'JobForm'

--- src/components/JobForm/JobForm.js
+++ src/components/JobForm/JobForm.js
@@ -12,6 +12,8 @@ import {
 // import { NavigationContainer } from '@react-navigation/native';
 // import { useNavigation } from '@react-navigation/native';
 import { Formik } from 'formik';
+// import { StatusBar } from 'expo-status-bar';
+import { openDatabase } from "react-native-sqlite-storage";
 
 const {width, height} = Dimensions.get('screen');
 
@@ -22,19 +24,67 @@ const styles = StyleSheet.create({
         height: 25,
         fontSize: 18
     }
-  });
+});
+
+const emptyInput = {
+    company: '',
+    title: '',
+    deadline: '',
+    applied: '',
+    link: ''
+}
 
 class JobForm extends Component {
 
     submit = (values) => {
-        console.log(values);
+        if (values !== emptyInput) {
+            console.log(values);
+            const db = openDatabase('table_applications.db')
+            db.transaction(function(tx){
+                tx.executeSql(
+                  //"create table if not exists DataTable (id integer primary key not null, column_1 int, column_2 int, column_3 text);",
+                  'CREATE TABLE IF NOT EXISTS table_applications(job_id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(225), company VARCHAR(225), deadline VARCHAR(40), applied VARCHAR(40), link VARCHAR(225));',
+                  []
+                );
+        
+                tx.executeSql(
+                  "INSERT INTO table_applications (title, company, deadline, applied, link) VALUES (?,?,?,?,?)",
+                  [values.title, values.company, values.deadline, values.applied, values.link],
+                  (tx, results) => {
+                      if (results.rowsAffected > 0) { //CHECK: alert isn't working insert console.log here
+                      console.log('ResultsAdded', results.rowsAffected);
+                        Alert.alert(
+                          'Success',
+                          'You\'re Job Application was Successfully Submitted',
+                          [
+                            {
+                              text: 'Ok',
+                              onPress: () => navigation.navigate('HomeScreen'),
+                            },
+                          ],
+                          { cancelable: false }
+                        );
+                      } else Alert.alert(
+                      'Error',
+                      'Submission Failed');
+                  }
+                );
+        
+                tx.executeSql(
+                  "select * from table_applications",
+                  [],
+                  (_, { rows: { _array } }) => console.log( _array),
+                  () => console.log("error fetching")
+                );s
+            });
+        }
     }
 
     renderForm = () => {
         return (
             <>
             <Formik
-                initialValues={{ company: '', title: '', deadline: '', applied: '', link: ''}}
+                initialValues={ emptyInput }
                 onSubmit={values => this.submit(values)}>
                 {({ handleChange, handleBlur, handleSubmit, values }) => (
                 <View>
@@ -79,9 +129,6 @@ class JobForm extends Component {
             </Formik>
             </>
         )
-    }
-    handleSubmit = () => {
-
     }
 
     render() {
