2f745fdd11ba4fa4088da8b076651176de2cf92e
erichan42 <erichan210hh@gmail.com>
2020-10-22 00:23:22 -0700

    Finished basic UI for 'AddJob' page

--- src/navigation/AddJob/AddJob.js
+++ src/navigation/AddJob/AddJob.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 import JobForm from './../../components/JobForm/JobForm'
+import HeaderBar from './../../components/HeaderBar/Headerbar'
+
+const {width, height} = Dimensions.get('screen');
 
 const AddJob = ({navigation}) => {
 
+  const Stack = createStackNavigator()
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <JobForm />
-    </View>
+    <>
+      <HeaderBar
+         navigation={Stack}
+         left="" leftNav=""
+         right="" rightNav="" />
+      <Text style={styles.text}>Add Job</Text>
+      <View style={styles.formContainer}>
+        <JobForm />
+      </View>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    margin: 12
+  },
+  formContainer: {
+    width: width - 24,
+    alignSelf: 'center',
+  }
+});
 
 export default AddJob;
 
