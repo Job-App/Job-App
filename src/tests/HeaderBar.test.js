e555a956006f993ba3129d5bbea79c9ac562b884
gyhyihe <47801373+gyhyihe@users.noreply.github.com>
2020-10-22 03:55:21 -0400

    Revert "random attempts to set up for jest"
    This reverts commit 692d1eee57cd9ead0ef08a1d524de4d381d3a726.

--- src/tests/HeaderBar.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import HeaderBar from '../components/HeaderBar/Headerbar.js';
-
-test('Testing HeaderBar component', () => {
- const wrapper = shallow(<HeaderBar />);
- expect(wrapper).toMatchSnapshot();
-});
