53ab22a53f4ea6af29639cc51142a2459d69bc28
erichan42 <erichan210hh@gmail.com>
2020-10-24 09:50:21 -0700

    Finished basic UI for the 'JobDetails' page

--- src/navigation/JobDetails/JobDetails.js
+++ src/navigation/JobDetails/JobDetails.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { TouchableOpacity, Text, View } from 'react-native'
+import { TouchableOpacity, Text, View, StyleSheet, Dimensions } from 'react-native'
 
+import { createStackNavigator } from '@react-navigation/stack'
 import { openDatabase } from 'react-native-sqlite-storage'
 
-var db = openDatabase('table_applications.db')
+import HeaderBar from './../../components/HeaderBar/Headerbar'
+
+const { width, height } = Dimensions.get('screen')
+const db = openDatabase('table_applications.db')
+
 const JobDetails = ({ route }) => {
+    const Stack = createStackNavigator()
     let [details, setDetails] = useState({})
 
     const sqlQuery =
@@ -27,13 +33,45 @@ const JobDetails = ({ route }) => {
     console.log(details)
     return (
         <View>
-            <Text>Job ID: {details.job_id || 'N/A'}</Text>
-            <Text>Company: {details.company || 'N/A'}</Text>
-            <Text>Title: {details.title || 'N/A'}</Text>
-            <Text>Deadline: {details.deadline || 'N/A'}</Text>
-            <Text>Applied: {details.applied || 'N/A'}</Text>
+            <HeaderBar
+                navigation={Stack}
+                left="Back"
+                leftNav="Homepage"
+                right=""
+                rightNav=""
+            />
+            {/* <Text>Job ID: {details.job_id || 'N/A'}</Text> */}
+            <Text style={[styles.title]}>{details.company || 'N/A'}</Text>
+            <Text style={[styles.subtitle]}>Title: {details.title || 'N/A'}</Text>
+            <Text style={[styles.text]}>Applied: {details.applied || 'N/A'}</Text>
+            <Text style={[styles.text]}>Deadline: {details.deadline || 'N/A'}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    text: {
+        margin: 24,
+        marginTop: 0,
+        marginBottom: 0,
+        fontSize: 18
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        margin: 24,
+        marginBottom: 0
+    },
+    subtitle: {
+        fontSize: 24,
+        fontStyle: 'italic',
+        margin: 24,
+        marginTop: 0,
+    },
+    formContainer: {
+        width: width - 48,
+        alignSelf: 'center',
+    },
+})
 
 export default JobDetails
