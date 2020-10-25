01bd1fc9960fadd6dbbd4c4a15e9ba224f2f585f
gyhyihe <47801373+gyhyihe@users.noreply.github.com>
2020-10-25 01:10:10 -0400

    random probing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,58 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx}', 'SQLite/**/*.{js,jsx}'],
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"]
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  preset: 'react-native',
+  verbose : true,
+  transformIgnorePatterns: [
+      "node_modules/(?!(jest-)?react-native)",
+      "node_modules/?!(react-navigation)"
+    ],
+    moduleDirectories: [
+            "node_modules",
+            "src"
+        ],
+  transform: {
+          "^.+\\.ts?$": "ts-jest",
+          "^.+\\.tsx?$": "ts-jest",
+          "^.+\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js",
+          "^.+\\.(css|scss|less)$": "jest-css-modules"
+        },
+ globals: {
+    "ts-jest": {
+     babelConfig: true,
+     tsConfig: {
+     allowJs: true,
+     },
+}
+}
 };
+
+/*
+  transform: {
+          "^.+\\.ts?$": "ts-jest",
+          "^.+\\.tsx?$": "ts-jest",
+          "^.+\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js",
+          "^.+\\.(css|scss|less)$": "jest-css-modules"
+        },
+          globals: {
+              "ts-jest": {
+                babelConfig: true,
+              }
+            }
+  transformIgnorePatterns: [
+
+      "node_modules/(?!(jest-)?react-native)",
+        "node_modules/?!(react-navigation)",
+              "node_modules/(?!(react-native"
+                + "|react-navigation-tabs"
+                + "|react-native-splash-screen"
+                + "|react-native-screens"
+                + "|react-native-reanimated"
+              + ")/)",
+
+    ],
+  */
--- src/tests/Cards.test.js
+++ src/tests/Cards.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Cards from '../components/Cards/Cards.js';
+import MockedNavigator from './MockedNavigator.js';
+import {render} from 'react-dom';
 
+it('should render correctly', () => {
+  const {toJSON} = render(
+    <MockedNavigator component={Cards} />
+  );
+  expect(toJSON()).toMatchSnapshot();
+});
+
+/*
 test('Testing Cards component', () => {
  const wrapper = shallow(<Cards />);
  expect(wrapper).toMatchSnapshot();
-});
+});*/
